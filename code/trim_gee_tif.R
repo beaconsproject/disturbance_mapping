574439fa018cbd404f497fa77cb96aa8f366e220
Pierre R Vernier <pierre.vernier@gmail.com>
2022-09-27 13:45:43 -0700

    Minor re-organization

--- code/trim_gee_tif.R
+++
@@ -1,9 +0,0 @@
-# Code to trim NA values from rasters downloaded from GEE
-library(terra)
-
-for (i in c(1984)) {
-    r <- rast(paste0('../gisdata/KDTT/raster30/kdtt_lc_',i,'.tif'))
-    NAflag(r) <- 0
-    rt <- trim(r)
-    writeRaster(rt, paste0('../gisdata/KDTT/raster30/lc_',i,'.tif'))
-}
